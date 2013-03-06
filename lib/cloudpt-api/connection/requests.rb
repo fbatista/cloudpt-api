2f216945564823aa63c53b93da297e213414d7f9
Fabio Batista <fbatista@webreakstuff.com>
2013-03-06 14:28:40 +0000

    removed debug output and bumped version

--- lib/cloudpt-api/connection/requests.rb
+++ lib/cloudpt-api/connection/requests.rb
@@ -9,8 +9,8 @@ module Cloudpt
           response = yield
           raise Cloudpt::API::Error::ConnectionFailed if !response
           status = response.code.to_i
-          puts "STATUS:#{status}"
-          puts "PAYLOAD:\n#{response.body}"
+          #puts "STATUS:#{status}"
+          #puts "PAYLOAD:\n#{response.body}"
           case status
             when 401
               raise Cloudpt::API::Error::Unauthorized
@@ -35,7 +35,7 @@ module Cloudpt
 
         def get_raw(endpoint, path, data = {}, headers = {})
           query = Cloudpt::API::Util.query(data)
-          puts "GET #{Cloudpt::API::Config.prefix}#{path}?#{URI.parse(URI.encode(query))}"
+          #puts "GET #{Cloudpt::API::Config.prefix}#{path}?#{URI.parse(URI.encode(query))}"
           request(:raw => true) do
             token(endpoint).get "#{Cloudpt::API::Config.prefix}#{path}?#{URI.parse(URI.encode(query))}", headers
           end
@@ -44,23 +44,23 @@ module Cloudpt
         def get(endpoint, path, data = {}, headers = {})
           query = Cloudpt::API::Util.query(data)
           query = "?#{URI.parse(URI.encode(query))}" unless query.empty?
-          puts "GET #{Cloudpt::API::Config.prefix}#{path}#{query}"
+          #puts "GET #{Cloudpt::API::Config.prefix}#{path}#{query}"
           request do
             token(endpoint).get "#{Cloudpt::API::Config.prefix}#{path}#{query}", headers
           end
         end
 
         def post(endpoint, path, data = {}, headers = {})
-          puts "POST #{Cloudpt::API::Config.prefix}#{path}"
-          puts "BODY:\n#{data}"
+          #puts "POST #{Cloudpt::API::Config.prefix}#{path}"
+          #puts "BODY:\n#{data}"
           request do
             token(endpoint).post "#{Cloudpt::API::Config.prefix}#{path}", data, headers
           end
         end
 
         def put(endpoint, path, data = {}, headers = {})
-          puts "PUT #{Cloudpt::API::Config.prefix}#{path}"
-          puts "BODY:\n#{data}"
+          #puts "PUT #{Cloudpt::API::Config.prefix}#{path}"
+          #puts "BODY:\n#{data}"
           request do
             token(endpoint).put "#{Cloudpt::API::Config.prefix}#{path}", data, headers
           end
--- lib/cloudpt-api/version.rb
+++ lib/cloudpt-api/version.rb
@@ -1,5 +1,5 @@
 module Cloudpt
   module API
-    VERSION = "0.1.2"
+    VERSION = "1.0.0"
   end
 end
